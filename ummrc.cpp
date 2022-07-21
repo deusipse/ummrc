736e1bdcc7f7383980b0d561d5e374caf4980c7a
deusipse <edwang512@gmail.com>
2022-07-21 21:52:55 +1000

    small cpp code update

--- ummrc.cpp
+++ ummrc.cpp
@@ -7,35 +7,36 @@
 #include <vector>
 
 using namespace std::chrono;
+using namespace std;
 
-std::random_device rd;
-std::mt19937 rng(rd());
+random_device rd;
+mt19937 rng(rd());
 
 const char particles[] = "XY";
 
-unsigned int num_threads = std::thread::hardware_concurrency();
+unsigned int num_threads = thread::hardware_concurrency();
 
-std::string gen_tube(int length) {
-  std::uniform_int_distribution<int> pick(0, 1);
-  std::string tube;
+string gen_tube(int length) {
+  uniform_int_distribution<int> pick(0, 1);
+  string tube;
   for (int i = 0; i < length; i++) {
     tube += particles[pick(rng)];
   }
   return tube;
 }
 
-std::string annihilate(std::string tube) {
-  std::string res = "";
+string annihilate(string tube) {
+  string output = "";
   for (char &c : tube) {
-    if (res.size() && res.back() == c && c == 'X')
-      res.pop_back();
+    if (output.size() && output.back() == c && c == 'X')
+      output.pop_back();
     else
-      res.push_back(c);
+      output.push_back(c);
   }
-  return res;
+  return output;
 }
 
-bool check_consec_x(std::string tube) {
+bool check_consec_x(string tube) {
   for (int i = 0; i < tube.length() - 1; i++) {
     if (tube[i] == tube[i + 1])
       return 0;
@@ -43,34 +44,24 @@ bool check_consec_x(std::string tube) {
   return 1;
 }
 
-double prob_consec(int length, int runs) {
-  long count = 0;
-  for (int i = 0; i < runs; i++) {
-    if (check_consec_x(gen_tube(length)))
-      count++;
-  }
-  return count/double(runs);
-}
-
 int main() {
   auto start = high_resolution_clock::now();
 
-  int runs = 10000000;
+  vector<future<string>> threads;
+  unsigned int len_tube = 1000000000;
 
-  // std::vector<std::future<long>> threads;
-  //
-  // for (int i = 8; i < num_threads+8; i++) {
-  //   threads.push_back(std::async(std::launch::async, count_check, i, runs));
-  // }
-  // for (auto &t : threads) {
-  //   std::cout << t.get() << '\n';
-  // }
-
-  for (int i = 8; i < 16; i++) {
-    std::cout << prob_consec(i, runs) << '\n';
+  string tube;
+  for (int i = 0; i < num_threads; i++) {                             // Multithreaded large annihilated tube
+    string small_tube = gen_tube(len_tube/num_threads);
+    threads.push_back(async(launch::async, annihilate, small_tube));
   }
+  for (auto &t : threads) {
+    tube += t.get();
+  }
+
+  cout << tube.length() << '\n';
 
   auto stop = high_resolution_clock::now();
   auto elapsed = duration_cast<milliseconds>(stop - start);
-  std::cout << elapsed.count() << "ms\n";
+  cout << elapsed.count() << "ms\n";
 }
