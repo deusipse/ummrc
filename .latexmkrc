b4a0a3f43b311b35832028426403fe480dc4748a
deusipse <edwang512@gmail.com>
2022-07-24 15:25:45 +1000

    added sweaty asy fig

--- .latexmkrc
+++ .latexmkrc
@@ -1,2 +1,7 @@
 $latex = 'latex --shell-escape %O %S';
 $pdflatex = 'pdflatex --shell-escape %O %S';
+
+sub asy {return system("asy -o asy/ '$_[0]'");}
+add_cus_dep("asy","eps",0,"asy");
+add_cus_dep("asy","pdf",0,"asy");
+add_cus_dep("asy","tex",0,"asy");
--- ummrc.cpp
+++ ummrc.cpp
@@ -27,12 +27,13 @@ string gen_tube(int length) {
 
 string annihilate(string tube) {
   string output = "";
-  for (char &c : tube) {
-    if (output.size() && output.back() == c && c == 'X')
-      output.pop_back();
-    else
-      output.push_back(c);
+  for (int i = 0; i < tube.length() - 1; i++) {
+    if (tube[i] != tube[i + 1] && tube[i] == 'X')
+      output += tube[i];
+    else if (tube[i] != 'X')
+      output += tube[i];
   }
+  output.push_back(tube.back());
   return output;
 }
 
@@ -44,22 +45,76 @@ bool check_consec_x(string tube) {
   return 1;
 }
 
-int main() {
-  auto start = high_resolution_clock::now();
+double prob_no_consec_after_n(int n, int runs) { // Q1
+  int count = 0;
+  for (int i = 0; i < runs; i++) {
+    if (check_consec_x(gen_tube(n)))
+      count++;
+  }
+  return double(count) / runs;
+}
 
+double average_len_after_n(int n, int runs) { // Q2
+  long long count = 0;
+  for (int i = 0; i < runs; i++) {
+    count += annihilate(gen_tube(n)).length();
+  }
+  return count / double(runs);
+}
+
+double threaded_avg_len_after_n(int n, int runs) {
+  double avg = 0;
+  vector<future<double>> threads;
+  for (int i = 0; i < num_threads; i++) {
+    threads.push_back(
+        async(launch::async, average_len_after_n, n, runs / num_threads));
+  }
+  for (auto &t : threads) {
+    avg += t.get();
+  }
+  return avg / num_threads;
+}
+
+double proportion_x(int n) {
+  string tube = "";
   vector<future<string>> threads;
-  unsigned int len_tube = 1000000000;
-
-  string tube;
-  for (int i = 0; i < num_threads; i++) {                             // Multithreaded large annihilated tube
-    string small_tube = gen_tube(len_tube/num_threads);
+  for (int i = 0; i < num_threads; i++) {
+    string small_tube = gen_tube(n / num_threads);
     threads.push_back(async(launch::async, annihilate, small_tube));
   }
   for (auto &t : threads) {
     tube += t.get();
   }
+  int len = tube.length();
+  return (len - 0.5 * n) / len;
+}
 
-  cout << tube.length() << '\n';
+int main() {
+  auto start = high_resolution_clock::now();
+
+  vector<future<double>> threads;
+  unsigned long long runs = 10000000;
+
+  // double avg = 0;
+  // for (int i = 0; i < num_threads; i++) {
+  //   threads.push_back(async(launch::async, average_len_after_n, 10,
+  //   runs/num_threads));
+  // }
+  // for (auto &t : threads) {
+  //   avg += t.get();
+  // }
+  // for (int i = 1; i <= 11; i++) {
+  //   cout << threaded_avg_len_after_n(i, runs) << '\n';
+  // }
+
+  // int count = 0;
+  // string tube = annihilate(gen_tube(100000));
+  // for (char &c : tube) {
+  //   if (c == 'Y')
+  //     count++;
+  // }
+
+  cout << proportion_x(runs) << '\n';
 
   auto stop = high_resolution_clock::now();
   auto elapsed = duration_cast<milliseconds>(stop - start);
