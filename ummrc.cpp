29737620e29dd96b083dee411bfbfcd529fbc7b3
deusipse <edwang512@gmail.com>
2022-07-24 17:58:01 +1000

    finished code without generalisation

--- ummrc.cpp
+++ ummrc.cpp
@@ -1,12 +1,10 @@
-#include <chrono>
-#include <future>
 #include <iostream>
+#include <thread>
+#include <future>
 #include <random>
 #include <string>
-#include <thread>
 #include <vector>
 
-using namespace std::chrono;
 using namespace std;
 
 random_device rd;
@@ -16,8 +14,8 @@ const char particles[] = "XY";
 
 unsigned int num_threads = thread::hardware_concurrency();
 
-string gen_tube(int length) {
-  uniform_int_distribution<int> pick(0, 1);
+string gen_tube(int length, double p) {
+  discrete_distribution<int> pick{p, 1 - p};
   string tube;
   for (int i = 0; i < length; i++) {
     tube += particles[pick(rng)];
@@ -39,16 +37,16 @@ string annihilate(string tube) {
 
 bool check_consec_x(string tube) {
   for (int i = 0; i < tube.length() - 1; i++) {
-    if (tube[i] == tube[i + 1])
+    if (tube[i] == tube[i + 1] && tube[i] == 'X')
       return 0;
   }
   return 1;
 }
 
-double prob_no_consec_after_n(int n, int runs) { // Q1
+double prob_no_two_consec_after_n(int n, int runs) { // Q1
   int count = 0;
   for (int i = 0; i < runs; i++) {
-    if (check_consec_x(gen_tube(n)))
+    if (check_consec_x(gen_tube(n, 0.5)))
       count++;
   }
   return double(count) / runs;
@@ -57,7 +55,7 @@ double prob_no_consec_after_n(int n, int runs) { // Q1
 double average_len_after_n(int n, int runs) { // Q2
   long long count = 0;
   for (int i = 0; i < runs; i++) {
-    count += annihilate(gen_tube(n)).length();
+    count += annihilate(gen_tube(n, 0.5)).length();
   }
   return count / double(runs);
 }
@@ -75,48 +73,36 @@ double threaded_avg_len_after_n(int n, int runs) {
   return avg / num_threads;
 }
 
-double proportion_x(int n) {
+double proportion_x(int runs, double p) {
   string tube = "";
   vector<future<string>> threads;
   for (int i = 0; i < num_threads; i++) {
-    string small_tube = gen_tube(n / num_threads);
+    string small_tube = gen_tube(runs / num_threads, p);
     threads.push_back(async(launch::async, annihilate, small_tube));
   }
   for (auto &t : threads) {
     tube += t.get();
   }
   int len = tube.length();
-  return (len - 0.5 * n) / len;
+  return (len - (1 - p) * runs) / len;
 }
 
 int main() {
-  auto start = high_resolution_clock::now();
 
-  vector<future<double>> threads;
-  unsigned long long runs = 10000000;
+  unsigned long long runs = 1000000;
 
-  // double avg = 0;
-  // for (int i = 0; i < num_threads; i++) {
-  //   threads.push_back(async(launch::async, average_len_after_n, 10,
-  //   runs/num_threads));
-  // }
-  // for (auto &t : threads) {
-  //   avg += t.get();
-  // }
-  // for (int i = 1; i <= 11; i++) {
-  //   cout << threaded_avg_len_after_n(i, runs) << '\n';
-  // }
+  cout << "q1\n";
+  for (int i = 1; i <= 10; i++) {
+    cout << "n = " << i << " prob = " << prob_no_two_consec_after_n(i, runs) << '\n';
+  }
+  cout << "q2\n";
+  for (int i = 1; i <= 10; i++) {
+    cout << "n = " << i << " avg length = " << threaded_avg_len_after_n(i, runs) << '\n';
+  }
+  cout << "q3\n";
+  for (double p = 0; p <= 10; p++) {
+    cout << "p = " << p/10 << " proportion = " << proportion_x(runs, p/10) << '\n';
+  }
 
-  // int count = 0;
-  // string tube = annihilate(gen_tube(100000));
-  // for (char &c : tube) {
-  //   if (c == 'Y')
-  //     count++;
-  // }
-
-  cout << proportion_x(runs) << '\n';
-
-  auto stop = high_resolution_clock::now();
-  auto elapsed = duration_cast<milliseconds>(stop - start);
-  cout << elapsed.count() << "ms\n";
+  return 0;
 }
